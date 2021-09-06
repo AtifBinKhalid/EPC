c5ee631e87314e0cad655cdd5c0ffe8d58be7e6c
Mukund Shukla <mukundshukla000@gmail.com>
2021-09-06 18:13:49 +0530

    new commit

---
+++ js/music.js
@@ -0,0 +1,32 @@
+  function dark() {
+  var audio = new Audio("images/start.mp3");
+  audio.volume = 0.3;
+  audio.play();
+  document.documentElement.classList.toggle('dark-mode');
+}
+  function play2() {
+  var audio = new Audio("images/pew.mp3");
+  audio.volume = 0.2;
+  audio.play();
+  document.getElementsByTagName('nav')[0].className = 'menu';
+}
+
+  function play1(){
+    var audio = new Audio("images/pew.mp3");
+    audio.volume = 0.2;
+    audio.play();
+    document.getElementsByTagName('nav')[0].className = 'menu menu_active';
+  }
+ function play3() {
+  var audio = new Audio("images/error.mp3");
+  audio.volume = 0.3;
+  audio.play();
+}
+function about() {
+  var audio = new Audio("images/about.mp3");
+  audio.volume = 0.7;
+  audio.play();
+}
+window.onload = function() {
+    document.getElementById("Audio").play();
+}
